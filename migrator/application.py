c8a427ebfe70c7c6e33fa4b805b62e39f188ad8f
Afonasev Evgeniy <afonasev@yandex-team.ru>
2016-09-29 12:56:38 +0500

    2

--- migrator/application.py
+++ migrator/application.py
@@ -36,7 +36,7 @@ class State(IState):
 
     JSON_INDENT = 4
 
-    def __init__(self, path: str, test=None):
+    def __init__(self, path: str):
         if os.path.exists(path):
             with open(path) as f:
                 self._data = json.load(f)
